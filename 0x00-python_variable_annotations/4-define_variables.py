d50137823dd3422bed90bfe1dfaafc8d2be4858b
Geff115 <gabrielnoah129@gmail.com>
2024-10-10 16:06:46 +0100

    Variable annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This script annotates some variables with some
+specified values.
+"""
+
+from typing import List, Dict, Tuple, Optional, Union
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
