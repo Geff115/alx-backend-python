c90cde87341f66b6cbcf60d83eac1b427fcb553d
Geff115 <gabrielnoah129@gmail.com>
2024-10-14 12:36:52 +0100

    Multiple coroutines with async, and sorting the batch in ascending order using insertion sort

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+This script imports wait_random from the previous python
+file, creates a async function called wait_n that takes two
+int arguments (n and max_delay), spawning wait_random n times
+with the specified max_delay. wait_n should return a list of all
+the delays (float values).
+"""
+
+import asyncio
+import random
+import importlib
+from typing import List
+
+
+# Dynamically import wait_random from '0-basic_async_syntax'
+basic_async_syntax = importlib.import_module('0-basic_async_syntax')
+wait_random = basic_async_syntax.wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    ARGS:
+        - n: an integer
+        - max_delay: an integer
+
+    RETURN: a floating point number
+    """
+    # running wait_random concurrently n times
+    list_delays = [wait_random(max_delay) for _ in range(n)]
+
+    # Gathering the result of the coroutines
+    batch = await asyncio.gather(*list_delays)
+
+    # Sorting the batch in a new list without using sorted()
+    # Using insertion sort algorithm
+    def insertion_sort(batch):
+        """
+        A helper function to help sort out the
+        batch list
+        """
+        sorted_list = []
+        for element in batch:
+            # finding the correct insertion point
+            i = 0
+            while i < len(sorted_list) and sorted_list[i] < element:
+                i += 1
+            # inserting the element at the correct position
+            sorted_list.insert(i, element)
+        return sorted_list
+
+    sorted_batch = insertion_sort(batch)
+
+    # Returning the new sorted batch list
+    return sorted_batch
