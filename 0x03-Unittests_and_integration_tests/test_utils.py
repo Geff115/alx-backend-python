dec60899bb706ce495d2e77dd05b553c61bb5b4e
Geff115 <gabrielnoah129@gmail.com>
2024-11-04 14:15:38 +0100

    Testing assertRaises to capture a KeyError

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,14 @@ class TestAccessNestedMap(unittest.TestCase):
                                path: Sequence, expected: Any) -> None:
         """Testing access nested map with different nested maps and path"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence) -> None:
+        """Testing that KeyError is raised for non-existent paths"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), repr(path[-1]))
